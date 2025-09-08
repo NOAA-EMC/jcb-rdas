cc63d3adadc515037786a67210b109033764747f
delippi <31075260+delippi@users.noreply.github.com>
2025-09-08 14:14:57 -0400

    Add Jinja-templated observation yamls and update configs for fv3jedi 3DEnVar ctest (#3)
    * Jinji-fy obs spaces, add 3denvar algorithm for ctest, update model/atm blocks, and update the configure file

--- test/client_integration/run.py
+++ test/client_integration/run.py
@@ -8,7 +8,8 @@ from wxflow import (parse_j2yaml, FileHandler, rm_p, logit,
                     Template, TemplateConstants)
 
 jedi_yaml = "./jedi.yaml"
-algorithm = "3dvar"
+#algorithm = "3dvar"
+#algorithm = "fv3jedi_3denvar"
 
 # Step 1: fill templates of the jcb base YAML file (use predefined for now)
 jcb_config = "rdas-atmosphere-templates.yaml"
@@ -17,7 +18,7 @@ with open(jcb_config, "r") as yaml_file:
 jcb_config = parse_j2yaml(jcb_config, task_config)
 
 # Step 2: (optional) fill templates of algorithm override YAML and merge
-jcb_config['algorithm'] = algorithm
+#jcb_config['algorithm'] = algorithm
 
 # Step 3: generate the JEDI Yaml using JCB driving YAML
 jedi_config = render(jcb_config)
